e316ee57afe56b798990be9c97990216a49e1279
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-13 13:50:42 +0800

    add sample dag

---
+++ littlest-airflow/ltaf/lib/api/binance.py
@@ -0,0 +1,23 @@
+import duckdb
+import logging
+logging.basicConfig(level=logging.INFO)
+
+__all__ = ['get_spot']
+
+def get_spot():
+
+    logging.info(duckdb.sql(
+        """
+            -- sql
+            CREATE OR REPLACE TABLE binance_spot AS
+            SELECT * FROM read_json('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+
+            -- sql
+            FROM binance_spot;
+        """
+    ))
+
+    logging.info("Created table binance_spot")
+
+if __name__ == "__main__":
+    print(get_spot())

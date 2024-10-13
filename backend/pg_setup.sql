e72191a740976d7d8d429e168640153f4ba6d392
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-13 13:08:09 +0800

    reformat folder for littlest-airflow

--- backend/pg_setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE airflow_db;
-CREATE USER airflow_user WITH PASSWORD 'airflow_pass';
-
--- Grant usage on the public schema
-GRANT USAGE ON SCHEMA public TO airflow_user;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO airflow_user;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO airflow_user;
-GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public TO airflow_user;
--- kill.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo $(AIRFLOW_HOME)
-kill $(cat $AIRFLOW_HOME/airflow-scheduler.pid)
-kill $(cat $AIRFLOW_HOME/airflow-webserver.pid)

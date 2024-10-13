2e448f2ebb2d6b07373df20fc02202e2d8796196
Dominic Leung <leungcheuk209@hotmail.com>
2024-10-13 16:56:48 +0800

    add binance spot dag

--- littlest-airflow/ltaf/lib/api/binance.py
+++ littlest-airflow/ltaf/lib/api/binance.py
@@ -10,15 +10,37 @@ def get_spot():
         """
             -- sql
             CREATE OR REPLACE TABLE binance_spot AS
-            SELECT * FROM read_json('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+                SELECT * FROM read_json('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
 
             -- sql
-            FROM binance_spot
-            SELECT *, STRFTIME(CURRENT_TIMESTAMP, '%Y-%m-%d %H:%M:%S') AS snapped_time;
+            CREATE OR REPLACE TABLE binance_spot AS
+                FROM binance_spot
+                SELECT *, CURRENT_TIMESTAMP AS snapped_time;
         """
     ))
 
     logging.info("Created table binance_spot")  
+
+    # TODO: postgres attach shall be moved to utils
+    duckdb.sql(
+        """
+            -- sql
+            INSTALL postgres;
+            LOAD postgres;
+            ATTACH 'dbname=postgres user=postgres password=postgres host=postgres-server port=5432' AS pg (TYPE POSTGRES, SCHEMA 'public_cache');
+
+            -- sql
+            CREATE TABLE IF NOT EXISTS pg.btc_spot AS
+                SELECT * FROM binance_spot LIMIT 0;
+
+            -- sql
+            INSERT INTO pg.btc_spot 
+            SELECT * FROM binance_spot;
+        """
+    )
+    logging.info("Inserted data into postgres")
+
+
 
 if __name__ == "__main__":
     print(get_spot())
